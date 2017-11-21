18495d9ab1fc1191676ddd3a310e9b7326e62ba8
tadkim <icanhear@dgu.edu>
2017-11-21 20:20:11 +0900

    add(feat, touch slider): add smooth scroll feature to WokrComponent's main image. this feature is for mobile only

--- src/app/main/touch-slider/touch-slider.component.ts
+++ src/app/main/touch-slider/touch-slider.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppService } from './../../app.service';
-
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-touch-slider',
@@ -13,11 +9,6 @@ import 'rxjs/add/operator/map';
 })
 export class TouchSliderComponent implements OnInit {
   contents;
-  @ViewChild('tsliders') tsliders;
-
-  scrollEvent$: Observable<any>;
-  scrollFn: Observable<any>;
-
 
   constructor(
     private appService: AppService,
@@ -28,27 +19,10 @@ export class TouchSliderComponent implements OnInit {
     this.contents = this.appService.getContents();
   }
 
-  ngAfterViewInit() {
-    let mouseStrem$ = Observable.fromEvent(this.tsliders.nativeElement, 'touchmove')
-      .subscribe(val => {
-        console.log(val);
-        return val;
-      });
-    
-    
-
-  }
-
   
   gotoDetail(id: number) {
     let link = ['/detail', id];
     this.router.navigate(link);
   }
-
-  onScroll(e: any) {
-    console.log("event", e);
-  }
-
-
 
 }
