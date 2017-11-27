327019321e465e1850624c6b80bad3e78b8b67f9
tadkim <icanhear@dgu.edu>
2017-11-27 17:20:50 +0900

    set(hello text interaction): change hello text opacity by scroll

--- src/app/main/hero/hero.component.ts
+++ src/app/main/hero/hero.component.ts
@@ -5,10 +5,6 @@ import 'rxjs/add/observable/fromEvent';
 
 import { getMap } from '../../utils/util';
 
-
-
-
-
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
@@ -22,7 +18,15 @@ export class HeroComponent implements OnInit {
   isHello: boolean = false;
 
 
-  styleConf = { opacity: 1 };
+  styleConf = {
+    opacity: 1
+  };
+
+  styleByScroll = {
+    scale: 'scale(1)',
+    paddingTop: '2.5rem'
+  };
+
   
 
   constructor(private router: Router) { }
@@ -41,10 +45,58 @@ export class HeroComponent implements OnInit {
 
   ngAfterViewInit() {
     let elementSize = { w: this.evtMouse.nativeElement.offsetWidth };
+    /*
     let mouseStrem$ = Observable.fromEvent<MouseEvent>(this.evtMouse.nativeElement, 'mousemove')
       .subscribe((res: any) => {
         this.styleConf.opacity = +getMap(res.offsetX, 0, elementSize.w, 0, 1).toFixed(1);
       }); 
+    */
+    this.registerScrollEvent();
+  }
+
+
+  // main image에 적용할 스크롤 이벤트
+  registerScrollEvent() {
+    
+    const imgHeight = { pc: 480, m: 375 };
+    const marginWithContent = 100;          // pc only.
+
+    const rule = {
+      pc: { startY: 0, endY: (imgHeight.pc + marginWithContent) },
+      m: { startY: 0, endY: imgHeight.m }
+    };
+    const scrollTop$ = Observable.fromEvent(window, "scroll")
+      .map((val: any) => {
+        let currentY = val.target.scrollingElement.scrollTop;
+        
+        if (currentY >= rule.pc.startY && currentY <= rule.pc.endY) {
+          let resultObj = { opacity: 0, scale: 'scale(1)' };
+
+          let result = getMap(
+            currentY,
+            rule.pc.startY,
+            rule.pc.endY/3,
+            1.0, 0.01);
+          
+          resultObj.opacity = +result.toFixed(1);
+          resultObj.scale = 'scale('
+            + getMap(currentY, rule.pc.startY, rule.pc.endY / 3, 1, .8) + ')';
+          
+          return resultObj;
+
+        } else {
+          return false;
+        }
+      }).distinctUntilChanged();
+    
+    // opacity effect 
+    const checkScroll$ = scrollTop$.subscribe((val:any) => {
+      this.styleConf.opacity = val.opacity; // hello text opacity
+      this.styleByScroll.scale = val.scale; // stuckyi image scale
+
+    });
+    
+    
   }
 
 
--- src/app/utils/util.ts
+++ src/app/utils/util.ts
@@ -1,6 +1,5 @@
 // p5js의 map()기능과 동일하다.
 export function getMap(val, start1, stop1, start2, stop2) {
-    console.log("getMax!");
     const newVal = ((val - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
     if (start2 < stop2) { 
         return Math.max(Math.min(newVal, stop2), start2);   // constrain
