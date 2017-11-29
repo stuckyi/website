6bce4165fb835910f551fca483fc3c316076f97a
tadkim <icanhear@dgu.edu>
2017-11-29 23:47:59 +0900

    chord(maintain)

--- src/app/main/footer/footer.component.ts
+++ src/app/main/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  emailUrl: string = 'hello@stuckyi.studio';
 
   constructor() { }
 
