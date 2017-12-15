9c99bad6211cd84fbcedea393f041e20ad1f3091
tadkim <icanhear@dgu.edu>
2017-12-16 01:44:55 +0900

    set(environment, change): move to another work environment. so, set another option to website work.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,6 @@ import { HttpModule } from '@angular/http';
 
 import { environment } from './../environments/environment';
 
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppService } from './app.service';
 
